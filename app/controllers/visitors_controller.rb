d1b50d4a9ffd5275f4811c7205e90ae56006e537
kschoonhoven <scnefu71@aol.com>
2015-11-06 19:59:33 +0100

    fixed issue with ssl

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -5,7 +5,7 @@ class VisitorsController < ApplicationController
   def update_visitor
 
     member_id = params[:id]
-    if spawn("casperjs  ~/Work/Projects/visit-counter/app/utils/fernwoodfitness_visitor_helper.js #{member_id}") > 0
+    if spawn("casperjs  --ignore-ssl-errors=true --ssl-protocol=any ~/app/utils/fernwoodfitness_visitor_helper.js #{member_id}") > 0
       render json: { status: :success, member_id: member_id }
     else
       render json: { status: :unprocessable_entity, errors: 'system error' }
