a90daeb6bd8769bd1b16b10a3df9d832a2ca3da1
kschoonhoven <scnefu71@aol.com>
2015-11-08 03:00:52 +0100

    heroku dyno launch

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -6,11 +6,11 @@ class VisitorsController < ApplicationController
 
     member_id = params[:id]
 
-    if system("casperjs --ignore-ssl-errors=true --ssl-protocol=any ~/app/utils/fernwoodfitness_visitor_helper.js #{member_id}")
+    #if system("casperjs --ignore-ssl-errors=true --ssl-protocol=any ~/app/utils/fernwoodfitness_visitor_helper.js #{member_id}")
       render json: { status: :success, member_id: member_id }
-    else
-      render json: { status: :unprocessable_entity, errors: 'system error' }
-    end
+    #else
+    #  render json: { status: :unprocessable_entity, errors: 'system error' }
+    #end
 
   end
 
